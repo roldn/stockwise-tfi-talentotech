418f96ac7cbd759bbce2dee1b08b0a37109b62e9
Francisco Roldán <roldanfrancisco.personal@gmail.com>
2026-07-10 18:04:41 -0300

    Remove menu definitions from config.py

--- config.py
+++ config.py
@@ -1,15 +1,1 @@
 DB_PATH = "trabajo-final-integrador/base de datos/inventario.db"
-
-MENU_PRODUCTOS = [
-    "1. Mostrar prodcutos",
-    "3. Encontrar prodcuto por ID",
-    "4. Registrar producto nuevo",
-    "5. Actualizar Productos",
-    "6. Salir"
-]
-
-MENU_CATEGORIAS = [
-    "1. Ver categorias",
-    "2. Crear nueva categoria",
-    "3. Eliminar Categoria"
-]
