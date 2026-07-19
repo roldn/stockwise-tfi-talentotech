fcc14aa3a5f31c48e56d3ca1781e100ed40ae38b
Francisco Roldán <roldanfrancisco.personal@gmail.com>
2026-07-18 21:43:54 -0300

    Refactor DB initialization and fix column types
    Refactor database initialization into a function and fix column type for 'cantidad'.

--- db/esquema.py
+++ db/esquema.py
@@ -1,26 +1,23 @@
-from .db_conexion import conexion
+from db.db_conexion import conexion
 
-with conexion() as conn:
-
-    conn.execute('''
-        CREATE TABLE IF NOT EXISTS categorias (
-            id INTEGER PRIMARY KEY UNIQUE AUTOINCREMENT,
-            nombre TEXT NOT NULL UNIQUE,
-            descripcion TEXT
-        )
-    ''')
-    
-    conn.execute('''
-        CREATE TABLE IF NOT EXISTS productos (
-            id INTEGER PRIMARY KEY UNIQUE AUTOINCREMENT,
-            nombre TEXT NOT NULL,
-            descripcion,
-            cantidad REAL,
-            precio REAL,
-            categoria_id INTEGER,
-            FOREIGN KEY (categoria_id) REFERENCES categorias(id)   
-        )
-    ''')
-
-    conn.commit()
-        
+def inicializar_db() -> None:
+    with conexion() as conn:
+        conn.execute('''
+            CREATE TABLE IF NOT EXISTS categorias (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre TEXT NOT NULL UNIQUE,
+                descripcion TEXT
+            )
+        ''')
+        conn.execute('''
+            CREATE TABLE IF NOT EXISTS productos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre TEXT NOT NULL,
+                descripcion TEXT,
+                cantidad INTEGER,
+                precio REAL,
+                categoria_id INTEGER,
+                FOREIGN KEY (categoria_id) REFERENCES categorias(id)   
+            )
+        ''')
+            
